6ba5033fa82c072dfbe529468cbbc28d66821c6a
akshpnwr <akashpanwar9976@gmail.com>
2021-03-17 21:31:45 +0530

    assignment 3

--- assignment-3/src/containers/Course/Course.js
+++ assignment-3/src/containers/Course/Course.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props);
+    console.log(new URLSearchParams(this.props.location.search));
+    return (
+      <div>
+        <h1>{new URLSearchParams(this.props.location.search).get("title")}</h1>
+        <p>
+          You selected the Course with ID:{" "}
+          {new URLSearchParams(this.props.location.search).get("id")}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Course;
